4f2162d78c2dbe28061dbbaf3492a569f7858bf5
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-04 12:56:37 -0700

    added color coding and styled the navabar icons

--- paw-pals/src/components/Navbar/index.js
+++ paw-pals/src/components/Navbar/index.js
@@ -6,23 +6,22 @@ function Navbar() {
     <div className='navbar'>
       <a className='navbar-link' href='/'>
         {" "}
-        <i className='fas fa-home'></i> Home
+        <i className='fas fa-home'></i>
       </a>{" "}
       <a className='navbar-link-1' href='/search'>
-        <i className='fas fa-search'></i> Search
+        <i className='fas fa-search'></i>
       </a>{" "}
       <a className='navbar-link-2' href='/adopt'>
         {" "}
-        <i className='fas fa-dog'></i> Adopt
+        <i className='fas fa-dog'></i>
       </a>{" "}
       <a className='navbar-link-3' href='/dog-parks'>
         {"   "}
         <i className='fas fa-cloud-sun'></i>
-        Dog Parks
       </a>
       {"  "}
       <a className='navbar-link-4' href='/profile'>
-        <i class='fas fa-user'></i>Profiles
+        <i class='fas fa-user'></i>
       </a>
     </div>
   );
