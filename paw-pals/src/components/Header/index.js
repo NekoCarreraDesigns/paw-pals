730755fd466273ca7dc5c670b74a82b0215aa1fc
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-05 16:46:50 -0700

    edited paragraphs on the intro page

--- paw-pals/src/components/Header/index.js
+++ paw-pals/src/components/Header/index.js
@@ -12,20 +12,21 @@ function Header() {
       <p className='intro-paragraph'>
         {" "}
         Welcome to Puppy Pals, your friendly and fun resource for finding
-        playmates for your pup, here you will find resources available to you: a
+        playmates for your pup! Here you will find resources available to you: a
         list of local animal shelters, if you are looking to adopt a best
-        friend, a map of the local dog parks to go on play dates, search
-        profiles of dogs, and users, as well as creating/editing your own
-        profile. Have fun and stay safe!
+        friend; as well as a map of the local dog parks to go on play dates. You
+        can also search profiles of dogs, and users, as well as create and edit
+        your own profile. Have fun and stay safe!
       </p>
       <p className='disclaimer-paragraph'>
         <strong>DISCLAIMER:</strong> Puppy Pals is not a dating site! Please
-        don't use it as one. We are here for our furry friends, so they can
+        don't use it as one. We are here for our furry friends so they can
         socialize with other dogs in fun and safe environments. What happens
-        between dog owners is between them, this behavior won't be tolerated,
-        users that violate this rule will be banned for life. We would also like
-        you to report abuses, and bad behavior off site to the local authorities
-        as well as our safety team. Remember to have fun and stay safe!
+        between dog owners is between them. This behavior will not be tolerated,
+        and users who violate this rule will be banned for life. We would also
+        like you to report abuses, and bad behavior off site to the local
+        authorities, as well as our safety team. Remember to have fun and stay
+        safe! Thanks for using Puppy Pals!
       </p>
       <button className='start-button' href='/profile'>
         Get Started!
