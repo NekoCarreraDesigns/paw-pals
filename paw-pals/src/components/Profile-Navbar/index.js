c168f104dacd1226d10fb7dec82d70124c611fbf
NekoCarreraDesigns <denvermaas@gmail.com>
2022-01-13 12:15:19 -0700

    changed route names in profile

--- paw-pals/src/components/Profile-Navbar/index.js
+++ paw-pals/src/components/Profile-Navbar/index.js
@@ -6,10 +6,10 @@ function ProfileNavbar() {
     <div className='profile-navbar'>
       <h1 className='profile-navbar-header'>View Other Users or Dogs</h1>
       <nav>
-        <a className='profile-navbar-link' href='/user'>
+        <a className='profile-navbar-link' href='/user-view'>
           <i class='fas fa-users'></i>
         </a>
-        <a className='profile-navbar-link' href='/dogs'>
+        <a className='profile-navbar-link' href='/dogs-view'>
           <i class='fas fa-paw'></i>
         </a>
       </nav>
